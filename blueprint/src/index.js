0d78257b9001cc533649a1b36a2496412f04a7e5
Trent Willis <twillis@linkedin.com>
2016-12-02 10:27:56 -0800

    Setup linting for blueprint

--- blueprint/src/index.js
+++ blueprint/src/index.js
@@ -1,9 +1,9 @@
 import Plugin from 'broccoli-plugin';
-import FSTree from 'fs-tree-diff';
-import heimdall from 'heimdalljs';
+import FSTree from 'fs-tree-diff'; // eslint-disable-line no-unused-vars
+import heimdall from 'heimdalljs'; // eslint-disable-line no-unused-vars
 import { default as _logger } from 'heimdalljs-logger';
 
-const logger = _logger('your-broccoli-plugin');
+const logger = _logger('your-broccoli-plugin'); // eslint-disable-line no-unused-vars
 
 export default class YourPlugin extends Plugin {
   constructor(node, options = {}) {
